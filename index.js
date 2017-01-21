aa4c6b9a7af3b5a5fa7de4f1b599d642476e28c7
Victor Volovik <andhope2die@gmail.com>
2017-01-21 16:14:57 +0300

    Add basic functionality
    Basic functionality:
      - create element;
      - render string inside element;
      - render props inside string;
      - render child elements.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const scarlet = () => {
-  return '<p></p>';
+const scarlet = (elem, str="", props={}, children=[]) => {
+  for (let prop in props) {
+    str = str.replace(new RegExp('{' + prop + '}', 'g'), props[prop]);
+  }
+  let childElems = children.join('');
+  return `<${elem}>${str}${childElems}</${elem}>`;
 };
 
 if ((typeof module !== 'undefined') && (typeof module.exports !== 'undefined')) {
--- test/basic-spec.js
+++ test/basic-spec.js
@@ -7,9 +7,52 @@ describe('Scarlet', () => {
   });
 
   describe('Create element', () => {
-    it('should return string with an element', () => {
+    it('should create an element', () => {
       const expected = '<p></p>';
       const actual = scarlet('p');
+      expect(actual).to.be.equal(expected);
+    });
+    it('should create another element', () => {
+      const expected = '<div></div>';
+      const actual = scarlet('div');
+      expect(actual).to.be.equal(expected);
+    });
+  });
+
+  describe('Render string', () => {
+    it('should render string inside element', () => {
+      const expected = '<div>Test</div>';
+      const actual = scarlet('div', 'Test');
+      expect(actual).to.be.equal(expected);
+    });
+  });
+
+  describe('Render props', () => {
+    it('should render props inside string', () => {
+      const user = {
+        name: 'Alfred'
+      };
+      const expected = '<p>My name is Alfred.</p>';
+      const actual = scarlet('p', 'My name is {name}.', user);
+      expect(actual).to.be.equal(expected);
+    });
+  });
+
+  describe('Render children', () => {
+    it('should render children elements', () => {
+      const dogs = [
+        {
+          name: 'Guffy',
+          age: 4
+        },
+        {
+          name: 'Pluto',
+          age: 2
+        }
+      ];
+      const expected = '<ul><li>Guffy is 4 years old.</li><li>Pluto is 2 years old.</li></ul>';
+      const dogsList = dogs.map(dog => scarlet('li', '{name} is {age} years old.', dog));
+      const actual = scarlet('ul', "", {}, dogsList);
       expect(actual).to.be.equal(expected);
     });
   });
